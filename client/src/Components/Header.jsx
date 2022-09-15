fbf70c948947be934968b06beb20005e91faeafe
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-15 17:35:56 +0100

    header set

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDownLong } from '@fortawesome/free-solid-svg-icons';
+
+function Header() {
+  return (
+    <header id="header" className='header'>
+        <h1 className='header-title'>
+            Tiffane Anlyou
+        </h1>
+
+        <div className='header-illustration'>
+            <img src="" alt="" className='header-illustration-img'/>
+        </div>
+
+        <div className='header-information'>
+            <span className='comment-span'>
+                //Who I am ?
+            </span>
+            <h2 className='header-information-activity'>
+                Software Engineer And Blogger
+            </h2>
+            <p className='header-information-p'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, nesciunt!
+            </p>
+            <a href="#" className='btn-godown'>
+                <FontAwesomeIcon icon={faArrowDownLong}/>
+            </a>
+        </div>
+    </header>
+)
+}
+
+export default Header
--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../Components/Header';
 import Navbar from '../Components/Navbar';
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
+            <Header/>
         </div>
     );
 }
